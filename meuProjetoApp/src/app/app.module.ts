377f9747812edb4913b06477c37d79c299a2ff21
Gabriel Albuquerque <biel123albuquerque@hotmail.com>
2023-07-07 01:07:29 -0400

    first commit adicionando angular material e alguns componentes

---
+++ meuProjetoApp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IndexComponent } from './index/index.component';
+import {HomeComponent} from "./home/home.component";
+import {MatIconModule} from "@angular/material/icon";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    IndexComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
