03beca6bf6a50bcbb234fe867300580f9aa62a70
Gabriel Albuquerque <biel123albuquerque@hotmail.com>
2023-08-22 18:04:39 -0400

    modifica método de login no auth.service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {map, Observable} from "rxjs";
 import {User} from "../models/User";
 
 @Injectable({
@@ -8,20 +8,24 @@ import {User} from "../models/User";
 })
 
 export class AuthService {
+  private TOKEN_KEY = 'jwt_token';
+
+  private token = this.getToken();
 
   SERVER_URL = 'http://localhost:8080/api'
 
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.token}`
+
     })
   };
 
-
   constructor(private http: HttpClient) {}
 
-    public getUsers() {
-      return this.http.get(`${this.SERVER_URL}/login/auth`)
+  getToken() {
+    return localStorage.getItem(this.TOKEN_KEY);
   }
 
   public register(name: string, email: string, password: string): Observable<any> {
@@ -33,11 +37,24 @@ export class AuthService {
     return this.http.post(`${this.SERVER_URL}/register`, data, this.httpOptions)
   }
 
-  public login(email: string, password: string): Observable<any> {
+  public login(email: string, password: string): Observable<void> {
     const data = {
       email: email,
       password: password
     };
     return this.http.post(`${this.SERVER_URL}/login`, data, this.httpOptions)
+      .pipe(
+        map((response: any) => {
+          if (response && response.token) {
+            localStorage.setItem('jwt_token', response.token);
+          } else {
+            throw new Error('Token JWT não encontrado na resposta.');
+          }
+        })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('jwt_token');
   }
 }
