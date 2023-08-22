904da5ae20de799b2499f5e12f97ea9b113943c9
Gabriel Albuquerque <biel123albuquerque@hotmail.com>
2023-08-22 15:52:31 -0400

    adiciona componente de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private auth: AuthService) {
+  }
+
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = 'Failed';
+
+  login(email: string, password: string) {
+    this.auth.login(email, password).subscribe(
+      (data) => {
+        this.isSuccessful = true
+        this.isSignUpFailed = false
+      },
+      (err) => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
+  }
 }
