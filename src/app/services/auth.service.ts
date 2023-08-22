87e9a06e9888dd3644c3f36a19899466c9ef5a5a
Gabriel Albuquerque <biel123albuquerque@hotmail.com>
2023-08-22 15:52:03 -0400

    Adiciona parte de login e registro do usuário

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../models/User";
 
 @Injectable({
   providedIn: 'root'
@@ -7,11 +9,35 @@ import {HttpClient} from "@angular/common/http";
 
 export class AuthService {
 
-  SERVER_URL = 'http://localhost:8080';
+  SERVER_URL = 'http://localhost:8080/api'
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
+
 
   constructor(private http: HttpClient) {}
 
     public getUsers() {
       return this.http.get(`${this.SERVER_URL}/login/auth`)
+  }
+
+  public register(name: string, email: string, password: string): Observable<any> {
+    const data = {
+      name: name,
+      email: email,
+      password: password
+    };
+    return this.http.post(`${this.SERVER_URL}/register`, data, this.httpOptions)
+  }
+
+  public login(email: string, password: string): Observable<any> {
+    const data = {
+      email: email,
+      password: password
+    };
+    return this.http.post(`${this.SERVER_URL}/login`, data, this.httpOptions)
   }
 }
