f9279fd172e038a3d7730308d3a7bce169c2ee15
Gabriel Albuquerque <biel123albuquerque@hotmail.com>
2023-08-22 15:40:44 -0400

    Adiciona forma de registro no componente passando os valores corretos para nome email e senha

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(private auth: AuthService) {
+  }
+
+    name: string= ''
+    email: string = ''
+    password: string = ''
+
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+  register() {
+    this.auth.register(this.name, this.email, this.password).subscribe(
+      (data) => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      (err) => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
+  }
 }
